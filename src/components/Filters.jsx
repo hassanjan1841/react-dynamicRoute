e8beb8e940936258e7d45688d524c162a80cc35c
Hassan Jan <161922826+hassanjan1841@users.noreply.github.com>
2024-09-05 12:27:43 +0000

    the filters button background hs been setup

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -4,31 +4,31 @@ function Filters({ onSortChange, chosenFilter }) {
     <div className="flex justify-start space-x-3 mb-8">
       <button
         onClick={() => onSortChange("all")}
-        className={`${chosenFilter == "all" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2 bg-white shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
+        className={`${chosenFilter == "all" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2  shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
       >
         All
       </button>
       <button
         onClick={() => onSortChange("a-z")}
-        className={`${chosenFilter == "a-z" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2 bg-white shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
+        className={`${chosenFilter == "a-z" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2  shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
       >
         A-Z
       </button>
       <button
         onClick={() => onSortChange("z-a")}
-        className={`${chosenFilter == "z-a" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2 bg-white shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
+        className={`${chosenFilter == "z-a" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2  shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
       >
         Z-A
       </button>
       <button
         onClick={() => onSortChange("low-to-high")}
-        className={`${chosenFilter == "low-to-high" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2 bg-white shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
+        className={`${chosenFilter == "low-to-high" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2  shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
       >
         Price: Low to High
       </button>
       <button
         onClick={() => onSortChange("high-to-low")}
-        className={`${chosenFilter == "high-to-low" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2 bg-white shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
+        className={`${chosenFilter == "high-to-low" ? "bg-slate-800 text-white" : "bg-white text-gray-700"} px-4 py-2  shadow-md hover:shadow-lg rounded-md text-gray-700 transition duration-300`}
       >
         Price: High to Low
       </button>
